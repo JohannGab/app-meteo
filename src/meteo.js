3ca532a1d40ab03d826f8621508d459b6f581b18
JohannGab <johann.gabelle@outlook.fr>
2020-08-03 20:36:30 +0200

    add géolocalisation

--- src/meteo.js
+++ src/meteo.js
@@ -1,15 +1,47 @@
 import { secret } from '../secret.js'
-console.log('test', secret);
 
+let ville;
+let options = {
+    enableHighAccuracy : true
+}
 
-let ville = "saint-saulve"
-recevoirTemperature(ville)
+if("geolocation" in navigator) {
+    navigator.geolocation.watchPosition((position) => {
+        const url = `http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${secret}&units=metric`
+            let requete = new XMLHttpRequest();
+            requete.open('GET', url);
+            requete.responseType = 'json';
+            requete.send();
+
+            requete.onload = function() {
+            if (requete.readyState === XMLHttpRequest.DONE) {
+                if (requete.status === 200) {
+                let reponse = requete.response;
+                let selectVille = reponse.name
+                let temperature = reponse.main.temp;
+                document.querySelector('#ville').textContent = selectVille
+                document.querySelector('#temperature_label').textContent = Math.round(temperature)
+                } else {
+                alert('Un problème est intervenu, merci de revenir plus tard.');
+                }
+            }
+        }
+    }, erreur, options);
+} else {
+    ville = 'Paris';
+    recevoirTemperature(ville);
+}
 
 const button = document.querySelector('#changer')
 button.addEventListener('click', () => {
     ville = prompt('Choisir une ville')
     recevoirTemperature(ville)
 })
+
+function erreur() {
+    ville = 'Paris';
+    recevoirTemperature(ville)
+}
 
 function recevoirTemperature() {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${ville}&APPID=${secret}&units=metric`
@@ -26,7 +58,6 @@ function recevoirTemperature() {
             let reponse = requete.response;
             let selectVille = reponse.name
             let temperature = reponse.main.temp;
-            console.log(temperature);
             document.querySelector('#ville').textContent = selectVille
             document.querySelector('#temperature_label').textContent = Math.round(temperature)
             } else {
@@ -34,6 +65,5 @@ function recevoirTemperature() {
             }
         }
     }
-    console.log(ville);
 }
 
